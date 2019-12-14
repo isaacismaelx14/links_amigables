447bec585e2ef9a646e5ea88340e499dddfbc504
isaacismaelx14 <48190882+isaacismaelx14@users.noreply.github.com>
2019-12-13 21:13:09 -0400

    change (not final v2)

--- views/index-view.php
+++ views/index-view.php
@@ -1,10 +1,8 @@
-<?php
-require("resource/request/head.php");
-
-?>
+<title>Home</title>
+<?php require("resource/request/head.php");?>
 <body class="">
     <h1>Este es el index</h1>
         <a href="login/">Login</a>
-    
+<?php require("resource/request/footer.php");?>
 </body>
 </html>
