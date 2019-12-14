dc3dbec48d34b3cc969d8edbe1d4ccf28e682c54
isaacismaelx14 <48190882+isaacismaelx14@users.noreply.github.com>
2019-12-14 06:59:49 -0400

    change (Not final v4.2)

--- resource/request/head.php
+++ resource/request/head.php
@@ -8,3 +8,4 @@ $app_name = "Smart Time";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=<?= $Css?>>
+
